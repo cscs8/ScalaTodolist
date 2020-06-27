ed6cb4f87df8d0803dd45db25c7c88c8c68ab7a2
cs8 <42040439+cscs8@users.noreply.github.com>
2020-06-27 22:34:20 +0900

    add case class

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,5 +1,19 @@
 package example
 
 object Main extends App {
-  println("Hello Scalaaaaaaooaaaaaaaaaaaaaaa!")
+  println(s"1 + 1 = ${1 + 1}")
+  println("Hello")
+  println({
+    val x = 1 + 1
+    x + 1
+  })
+  val p = Point(1, 3)
+  println(Point(1, 2))
+  println(p.toString)
+}
+
+case class Point(x: Int, y: Int) {
+  override def toString: String = {
+    s"Point dayo $x $y"
+  }
 }
